bc8ea48bc6960db0b220227f62f19877ce4d3f1d
Juna <itujun7@gmail.com>
2025-08-23 12:41:42 +0700

    Langkah 3.8 Seeding roles table

---
+++ db/migrations/20250822144230_seeding_roles_table.down.sql
@@ -0,0 +1,5 @@
+DELETE FROM roles WHERE id 
+    IN ("92be8d1a-5828-4218-98a8-19067aada49d",
+    "ab38f8c8-f07e-4e29-8f2a-fbde23f60050",
+    "e0f0d9c0-e402-4e0d-965f-9abf53301f79",
+    "8cc9981c-99d7-43f7-bd23-e4cfcd7d45fe");
---
+++ db/migrations/20250822144230_seeding_roles_table.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO roles (id, name, description) 
+VALUES ("92be8d1a-5828-4218-98a8-19067aada49d", "super_admin", "Super Admin"),
+        ("ab38f8c8-f07e-4e29-8f2a-fbde23f60050", "admin", "Admin"),
+        ("e0f0d9c0-e402-4e0d-965f-9abf53301f79", "seller", "Seller"),
+        ("8cc9981c-99d7-43f7-bd23-e4cfcd7d45fe", "buyer", "Buyer");
